e8d8942d2002d473764957484851ab2baf1e9ea2
DhuySoS <154112386+DhuySoS@users.noreply.github.com>
2024-01-07 14:27:20 +0700

    sign in & sign up with email password

---
+++ app/src/main/java/com/example/drinkfoodshop/home/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.drinkfoodshop.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.drinkfoodshop.R;
+import com.example.drinkfoodshop.loginAndRegister.intro;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnSignout;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignout = findViewById(R.id.btnLogOut);
+        mAuth = FirebaseAuth.getInstance();
+        btnSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MainActivity.this, intro.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/drinkfoodshop/loginAndRegister/intro.java
+++ app/src/main/java/com/example/drinkfoodshop/loginAndRegister/intro.java
@@ -7,11 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.drinkfoodshop.R;
 
 public class intro extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +20,7 @@ public class intro extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.intro);
+
     }
 
     public void dangKy(View view) {
